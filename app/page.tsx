4ccb5aa51882ca38a071d2f12f1b982176f80b4d
POM <peter@omalley.io>
2025-10-09 00:32:11 +0200

    eeep

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
+import { Suspense } from 'react'
 import { Hero } from '@/components/Hero'
 
 export default function Home() {
-  return <Hero />
+  return (
+    <Suspense fallback={<div className="h-[100dvh] bg-black" />}>
+      <Hero />
+    </Suspense>
+  )
 }
 
